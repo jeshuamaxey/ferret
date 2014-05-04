e1aa59c5f0c07d208230a3ce82edcc8b7f51018f
giovannic <giovanni.charles@gmail.com>
2014-05-04 14:14:17 +0100

    start on tweet pulling

--- low-pass/data/twitter.js
+++ low-pass/data/twitter.js
@@ -1,13 +1,38 @@
 var fs = require('fs');
 var path = require('path');
+var Twit = require('twit');
 
 var twitter = {
   
+  _T: null, 
+
   get key() {
     var file = path.join (__dirname, 'twitterapi.json');
     var data = fs.readFileSync(file, 'utf8');
     return JSON.parse(data);
+  },
+
+  get T() {
+    if (!this._T){
+      this._T = new Twit({
+            consumer_key: this.key.key,
+            consumer_secret: this.key.keySecret,
+            access_token: this.key.token,
+            access_token_secret: this.key.tokenSecret
+          });
+
+    }
+    return this._T;
+  },
+
+  getTweets: function(search, date, pages, cb){
+    //TODO: pages
+    this.T.get('search/tweets', { q: search }, function(err, data, res){
+      //lots of copying
+      cb(err, data.statuses);
+    });
   }
+
 
 };
 
--- low-pass/tests.js
+++ low-pass/tests.js
@@ -16,6 +16,19 @@ suite.addBatch({
         assert.isString(topic.key.keySecret);
         assert.isString(topic.key.token);
         assert.isString(topic.key.tokenSecret);
+      },
+
+      'can connect to api': function(topic){
+        assert.isObject(topic.T);
+      },
+
+      'can pull a page of tweets': {
+        topic: function () {
+                 twitter.twitter.getTweets('twitter', null, 1, this.callback)
+               },
+        'something is returned' : function (err, tweets) {
+          assert.isTrue(tweets.length > 0);
+        }
       }
     }
   }
