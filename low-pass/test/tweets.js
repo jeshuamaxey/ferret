52ef47c8be9d7da96a80c83ac405dbf7ec6768c4
giovannic <giovanni.charles@gmail.com>
2014-07-06 10:53:47 +0100

    test for retrieving older tweets

--- low-pass/test/tweets.js
+++ low-pass/test/tweets.js
@@ -3,6 +3,7 @@ var twitter = require('../data/twitter');
 
 describe('twitter', function(){
   describe('#getCachedTweetsFromDate', function(){
+
     it('should get tweets from today', function(done){
       var today = new Date();
       twitter.getCachedTweetsFromDate('twitter', Date.now(), 1)
@@ -14,6 +15,21 @@ describe('twitter', function(){
             tweetDate.getDate().should.equal(today.getDate());
             tweetDate.getMonth().should.equal(today.getMonth());
             tweetDate.getFullYear().should.equal(today.getFullYear());
+          }
+        }).then(done).fail(done);
+    });
+
+    it('should get tweets from five days ago', function(done){
+      var day = new Date(Date.now() - 5*24*60*60*1000);
+      twitter.getCachedTweetsFromDate('twitter', day.getTime(), 1)
+        .then(function(tweets){
+          var statuses = tweets;
+          statuses.length.should.be.above(0);
+          for(var t in statuses){
+            var tweetDate = new Date(statuses[t].created_at);
+            tweetDate.getDate().should.equal(day.getDate());
+            tweetDate.getMonth().should.equal(day.getMonth());
+            tweetDate.getFullYear().should.equal(day.getFullYear());
           }
         }).then(done).fail(done);
     });
