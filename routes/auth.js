9b8f865af66deee63fd928d423fda24ef17965e4
giovannic <giovanni.charles@gmail.com>
2014-07-07 15:09:10 +0100

    oauth until sessions

--- routes/auth.js
+++ routes/auth.js
@@ -2,13 +2,18 @@ var express = require('express');
 var router = express.Router();
 
 var passport = require('passport')
-var OAuth2Strategy = require('passport-oauth').OAuth2Strategy;
+var OAuthStrategy = require('passport-oauth').OAuthStrategy;
 
-passport.use('provider', new OAuth2Strategy({
+passport.use('twitter', new OAuthStrategy({
     //authorizationURL: 'https://www.provider.com/oauth2/authorize',
-    tokenURL: 'https://api.twitter.com/oauth/request_token',
-    callbackURL: 'http://localhost:3000/auth/twitter/callback'
+    userAuthorizationURL: 'https://api.twitter.com/oauth/authorize',
+    requestTokenURL: 'https://api.twitter.com/oauth/request_token',
+    accessTokenURL: 'https://api.twitter.com/oauth/access_token',
+    callbackURL: 'http://localhost:3000/auth/twitter/callback',
+    consumerKey: '1',
+    consumerSecret: '1'
   },
+
   function(accessToken, refreshToken, profile, done) {
     console.log('access token:' + accessToken);
     done(null, null);
@@ -19,14 +24,14 @@ passport.use('provider', new OAuth2Strategy({
 // Redirect the user to the OAuth 2.0 provider for authentication.  When
 // complete, the provider will redirect the user back to the application at
 //     /auth/provider/callback
-router.get('/twitter', passport.authenticate('provider'));
+router.get('/twitter', passport.authenticate('twitter'));
 
 // The OAuth 2.0 provider has redirected the user back to the application.
 // Finish the authentication process by attempting to obtain an access
 // token.  If authorization was granted, the user will be logged in.
 // Otherwise, authentication has failed.
 router.get('/twitter/callback', 
-  passport.authenticate('provider', { successRedirect: '/',
+  passport.authenticate('twitter', { successRedirect: '/',
                                       failureRedirect: '/' }));
 
 module.exports = router;
