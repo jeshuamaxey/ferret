b638aa6c708a3b600013b451524502a4761aaf8b
giovannic <giovanni.charles@gmail.com>
2014-07-07 18:00:02 +0100

    passport requirement

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var session = require('express-session');
 var bodyParser = require('body-parser');
+var passport = require('passport');
 
 var routes = require('./routes/index');
 var api = require('./routes/api');
