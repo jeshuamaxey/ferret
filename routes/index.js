c2b5ba98dcc28a102a12132a57446d0bcc228f75
Jeshua Maxey <me@jeshua.co>
2014-07-10 11:49:47 +0100

    added scaffold of about page and route

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,14 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res) {
 	res.render('index', {
-		'title': 'Low Pass'
+		'title': 'Ferret'
+	});
+});
+
+/* GET about page. */
+router.get('/about', function(req, res) {
+	res.render('about', {
+		'title': 'About Ferret'
 	});
 });
 
